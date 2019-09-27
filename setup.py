745e34f01ec849e76e7ae585f1ae4b88a8e19b81
Francesco Virga <francescod.virga@gmail.com>
2019-09-27 17:41:24 -0400

    Score word

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def init_board():
     board[1,1] = board[2,2] = board[3,3] = board[4,4] = board[4,10] = \
     board[3,11] = board[2,12] = board[1,13] = board[10,4] = board[11,3] = \
     board[12,2] = board[13,1] = board[10,10] = board[11,11] = board[12,12] = \
-    board[13,13] = \
+    board[13,13] = board[7,7] = \
     {'letter': '', 'multiplier': 2, 'isLetter': False}
 
     # double letter multiplier
@@ -35,5 +35,6 @@ def init_board():
     board[8,8] = board[8,12] = board[11,0] = board[11,7] = board[11,14] = \
     board[12,6] = board[12,8] = board[14,3] = board[14,11] = \
     {'letter': '', 'multiplier': 2, 'isLetter': True}
+
 
     return board
