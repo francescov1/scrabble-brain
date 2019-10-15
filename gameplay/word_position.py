12aeac706c9a32a3c9fd864a9178aff9e5a5e0ed
lauraturner <37046209+lauraturner@users.noreply.github.com>
2019-10-15 11:53:59 -0400

    Add documentation to word ranking files

--- gameplay/word_position.py
+++ gameplay/word_position.py
@@ -1,5 +1,5 @@
-import string 
-
+# given the current state of the board find all viable tiles to play a word around 
+# (ex. a tile without tiles on its left or right, or top and bottom)
 def word_position(board):
     word_indx = []
     playable = []
@@ -7,7 +7,6 @@ def word_position(board):
         for j in range(0, len(board)):
             if board[i][j][0].isspace() and not board[i][j][1].isspace():
                 word_indx.append([i, j])
-    
     for pair in word_indx:
         i = pair[0]
         j = pair[1]
